6dd1147625363fc597e17bce83f1280bbecebe4e
nande <nandoaj20@gmail.com>
2018-11-08 20:30:59 -0500

    commit es perpetrar

--- app/src/main/java/com/example/tanrasan/githubprueba/MainActivity.java
+++ app/src/main/java/com/example/tanrasan/githubprueba/MainActivity.java
@@ -4,8 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-    @Override
+    public class MainActivity extends AppCompatActivity {
+        private EditText et1;
+        private EditText et2;
+        private EditText et3;
+        private TextView tv1;
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
